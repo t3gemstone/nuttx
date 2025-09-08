954eaacb1766eef9fc43cea23f6b4cb6e459721a
Abdullah TURKMEN <abdullahturkmen@protonmail.com>
2025-09-08 08:25:54 +0300

    arch/am67_clockconfig clockconfig is dissabled
    Now The clock is set by U-boot. We don't need set anymore in here
    
    Signed-off-by: abdunaber <abdullahturkmen@protonmail.com>

--- arch/arm/src/am67/am67_clockconfig.c
+++ arch/arm/src/am67/am67_clockconfig.c
@@ -31,10 +31,11 @@
 #include "arm_internal.h"
 #include "am67_clockconfig.h"
 #include "hardware/am67_memorymap.h"
-
+#include <nuttx/arch.h>
 #include "am67_timer.h"
 #include "am67_irq.h"
 #include "irq/irq.h"
+
 
 /****************************************************************************
  * Private Functions
@@ -43,134 +44,12 @@
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
+#include <stdint.h>
 
-struct clock_ctrl gclock_ctrl;
-struct clock_conf gclock_conf =
-{
-    .period_usec = 100,
-    .base_addr = TIMER0_BASE_ADDR,
-    .hw_intr_num = 24,
-    .clock_hz = 25000000,
-    .prescaler = 1
-};
 
-void clock_unlock(void)
-{
-    uint32_t base_addr;
-    volatile uint32_t *kick_addr;
 
-    base_addr = CSL_CTRL_MMR0_CFG0_BASE;
-    kick_addr = (volatile uint32_t *)(base_addr + CSL_MAIN_CTRL_MMR_LOCKn_KICK0_OFFSET(2));
-    CSL_REG32_WR(kick_addr, KICK0_UNLOCK_VAL);
-    kick_addr++;
-    CSL_REG32_WR(kick_addr, KICK1_UNLOCK_VAL);
-}
 
-void clock_lock(void)
-{
-    uint32_t base_addr;
-    volatile uint32_t *kick_addr;
-    
-    base_addr = CSL_CTRL_MMR0_CFG0_BASE;
-    kick_addr = (volatile uint32_t *)(base_addr + CSL_MAIN_CTRL_MMR_LOCKn_KICK0_OFFSET(2));
-    CSL_REG32_WR(kick_addr, KICK_LOCK_VAL);
-    kick_addr++;
-    CSL_REG32_WR(kick_addr, KICK_LOCK_VAL);
-}
 
-int timer_tick_isr(int irq, void *context, void *arg)
-{
-    gclock_ctrl.ticks++;
-
-    if (gclock_ctrl.clock.timeout  == gclock_ctrl.ticks)
-    {
-        gclock_ctrl.clock.timeout = 0;
-        
-        if (gclock_ctrl.clock.period != 0U)
-            gclock_ctrl.clock.timeout = gclock_ctrl.ticks + gclock_ctrl.clock.period;
-    }
-    clear_overflow_int(gclock_ctrl.base_addr);
-    
-    return OK;
-}
-
-void clock_stop(void)
-{
-    // Implement later if needed
-}
-
-void clock_start(void)
-{
-    if (gclock_ctrl.clock.timeout > 0U) clock_stop(); // If timer is active, restart with new value
-    
-    gclock_ctrl.clock.timeout = (uint32_t)gclock_ctrl.ticks + gclock_ctrl.clock.start_timeout;
-}
-
-void clock_construct(void)
-{    
-    gclock_ctrl.clock.callback = NULL;
-    gclock_ctrl.clock.args = NULL;
-    gclock_ctrl.clock.timeout = 10;
-    gclock_ctrl.clock.period = 100;
-    gclock_ctrl.clock.start_timeout = 0;
-}
-
-void clock_init(void)
-{
-    struct timer timer_params;
-    struct intr intr_params;
-    
-    gclock_ctrl.ticks = 0;
-    gclock_ctrl.period_usec = gclock_conf.period_usec;
-    gclock_ctrl.base_addr = gclock_conf.base_addr;
-
-    // Default parameters
-    timer_params.clock_hz = 25 * 1000000;
-    timer_params.prescaler = 1;
-    timer_params.period_usec = 1000;
-    timer_params.one_shot = 0;
-    timer_params.overflow_intr = 1;		// Enable interrupts
-    timer_params.dma = 0;
-    
-    // Configuration parameters
-    timer_params.prescaler = gclock_conf.prescaler;
-    timer_params.clock_hz = gclock_conf.clock_hz;
-    timer_params.period_usec = gclock_conf.period_usec;
-    
-    timer_setup(gclock_ctrl.base_addr, &timer_params);
-
-    // gclock_ctrl.reload_count = get_reload(gclock_ctrl.base_addr); // Reading this value stops timer ?
-    
-    // Default parameters
-
-    intr_params.intr_num = 0;
-    intr_params.callback = NULL;
-    intr_params.args = NULL;
-    intr_params.event_id = 0;
-    intr_params.priority = (INTR_MAX_PRIORITY - 1U);
-    // intr_params.is_fiq = 0;
-    // intr_params.is_pulse = 0;
-
-    intr_params.intr_num = gclock_conf.hw_intr_num;
-    intr_params.callback = timer_tick_isr;
-
-    disable_intr(intr_params.intr_num);
-    clear_intr(intr_params.intr_num);
-    intr_set_priority(intr_params.intr_num, intr_params.priority);
-
-    set_vector((uint32_t)intr_params.intr_num, (uintptr_t)timer_tick_isr);
-
-    g_irqvector[intr_params.intr_num].handler = intr_params.callback;
-    g_irqvector[intr_params.intr_num].arg = intr_params.args;
-
-    enable_intr(intr_params.intr_num);
-
-    timer_start(gclock_ctrl.base_addr);
-}
-
-void clock_deinit(void)
-{
-    timer_stop(gclock_ctrl.base_addr);
-
-    // Do some interrupt related things here
+void am67_clockconfig(){
+    // all periphal clocks are setted by U-boot so we don't need to setup
 }
--- arch/arm/src/am67/am67_clockconfig.h
+++ arch/arm/src/am67/am67_clockconfig.h
@@ -35,30 +35,6 @@
  ****************************************************************************/
 
 
-// MCU domain
-
-#define MCU_TIMER0_CLOCK_SRC_MUX_ADDR       (0x4509060u)
-#define MCU_TIMER0_CLOCK_SRC_HFOSC0_CLKOUT  (0x0u)
-#define MCU_TIMER0_BASE_ADDR                (0x4800000u)
-
-// MAIN domain
-
-#define TIMER0_CLOCK_SRC_MUX_ADDR		(0x1081B0u)
-#define TIMER0_CLOCK_SRC_HFOSC0_CLKOUT		(0x0u)
-#define TIMER0_BASE_ADDR			(0x2400000u)
-
-#define CSL_CTRL_MMR0_CFG0_BASE			(0x100000UL)
-
-#define CSL_MCU_CTRL_MMR0_CFG0_BASE         (0x4500000UL)
-#define CSL_MCU_CTRL_MMR0_CFG0_SIZE         (0x20000UL)
-
-#define CSL_MAIN_CTRL_MMR_LOCKn_KICK0_OFFSET(n)     (0x1008 + 0x4000 * (n))
-#define CSL_MCU_CTRL_MMR_LOCKn_KICK0_OFFSET(n)      (0x1008 + 0x4000 * (n))
-
-#define KICK_LOCK_VAL                       (0x00000000U)
-#define KICK0_UNLOCK_VAL                    (0x68EF3490U)
-#define KICK1_UNLOCK_VAL                    (0xD172BC5AU)
-
 #ifdef __cplusplus
 extern "C"
 {
@@ -68,49 +44,8 @@ extern "C"
  * Public Types
  ****************************************************************************/
 
-typedef int (*fxn_callback)(int irq, void *context, void *args);
 
-struct clock
-{
-    fxn_callback callback;
-    void *args;
-    uint32_t start_timeout;
-    uint32_t timeout;
-    uint32_t period;
-    // struct clock *next; Uncomment to convert to a linked list
-};
 
-struct clock_ctrl
-{
-    uint32_t ticks;
-    struct clock clock;
-    uint32_t period_usec;
-    uint32_t rsv[32u / sizeof(uint32_t)];
-    uint32_t base_addr;
-    uint32_t reload_count;
-};
-
-struct clock_conf
-{
-    uint32_t base_addr;
-    uint32_t hw_intr_num;
-    uint32_t event_id;
-    uint32_t clock_hz;
-    uint32_t prescaler;
-    uint32_t period_usec;
-};
-
-/****************************************************************************
- * Public Function Prototypes
- ****************************************************************************/
-
-void clock_init(void);
-void clock_deinit(void);
-void clock_unlock(void);
-void clock_lock(void);
-void clock_start(void);
-void clock_construct(void);
-int timer_tick_isr(int irq, void *context, void *arg);
 
 #ifdef __cplusplus
 }
